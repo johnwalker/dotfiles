fc02e213c747e7ed795b2df5ade7d60a84794726
John Walker <john.lou.walker@gmail.com>
2014-05-17 14:02:19 -0400

    Add auctex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,9 @@
 
 (package-initialize)
 
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
 
 (setq package-list '(ac-nrepl
 		     ag
